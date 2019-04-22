db7700359e17dc9f39fc4cdecdc517bd017b3007
Jeferson <mx.jeferson.12@gmail.com>
2019-04-22 15:30:08 -0300

    desafio 102: add colors

--- Desafios/desafio 104 - help cores.py
+++ Desafios/desafio 104 - help cores.py
@@ -3,18 +3,35 @@ from time import sleep
 print('=' * 20,'DESAFIO 104 - HELPER PYTHON')
 
 
-def titulo(msg):
+def cores(color):
+    cor = {
+        'limpa': '\33[m',
+        'vermelho': '\33[1;30;41m',
+        'amarelo': '\33[30;43m',
+        'azul': '\33[1;30;44m'
+    }
+
+    return cor[color]
+
+
+def titulo(msg, cor):
     tam = len(msg) + 4
+    print(cores(cor),end='')
     print(f'~' * tam)
     print(f'  {msg}')
     print(f'~' * tam)
+    print(cores('limpa'),end='')
 
 
-titulo('PY HELPER - ajuda com comandos')
+titulo('PY HELPER - ajuda com comandos','amarelo')
+
 
 helper = ''
 while helper != 'sair':
     helper = input('ajuda ou duvida> ').lower()
-    titulo('Aguarde...')
     sleep(1)
+    titulo('Aguarde...', 'vermelho')
+    sleep(2)
+    print(cores('azul'))
     help(helper)
+    print(cores('limpa'))
